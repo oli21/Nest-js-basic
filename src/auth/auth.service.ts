8a89fd6ebbe2b8dee4084300c74938172866627a
rubaith <rubaith95@gmail.com>
2023-03-02 19:06:22 +0600

    added sign up logic

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma:PrismaService) {
+    
+  }
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          bookmarks: { create: { link: 'hello', title: 'hello' } },
+        },
+      });
+      const users = await this.prisma.user.findMany({
+        include: { bookmarks: true },
+      });
+      const bookmarks = await this.prisma.bookmark.findMany();
+
+      return { users, bookmarks };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('email already exists');
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  signin() {
+    return { msg: 'I am signin' };
+  }
+}
